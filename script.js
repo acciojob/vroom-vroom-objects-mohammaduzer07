00dbd2c02d0a534df0c5b806020d7e8478c1ab17
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-06-03 15:43:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 // Complete the js code
 function Car(make, model) {
-	constructor(make, model){
-		this._make = make;
-		this._model = model;
-	}
-
-	get getMakeModel(){
-		return (this._make + " "+ this._model);
-	}
-	
+	this.make = make;
+	this.model = model;
+}
+Car.prototype.getMakeMode  = function (){
+	return `${this.make} ${this.model}`;
 }
 
-function SportsCar extends Car(make, model, topSpeed) {
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+}
 
-	constructor(make, model, topSpeed ){
-		super(make, model);
-		this._topSpeed = topSpeed;
-	}
-	get getTopSpeed(){
-		return this._topSpeed;
-	}
+Object.setPrototypeOf(SportsCar.prototype, Car.prototype);
+
+SportsCar.prototype.getTopSpeed = function () {
+	return this.topSpeed;
 }
 
 // Do not change the code below
